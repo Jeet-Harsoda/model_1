3eb17433c2939757930241fe63a3655a945a17b2
Jeet Harsoda <harsodajeet666@gmail.com>
2026-06-12 08:28:22 +0530

    fixes

--- react_app/back_end/index.js
+++ react_app/back_end/index.js
@@ -38,7 +38,7 @@ app.post("/submit-register", async (req, res) => {
     }
 });
 
-app.get('/*', (req, res) => {
+app.get('/:path*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
